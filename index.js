1b17cfb46fcb7af3ac8784a810864ec3233e4dfb
bosees <rladuddlf3@naver.com>
2020-07-19 20:38:51 +0900

    nomad:first express server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+const port = 3333;
+
+function handleListening() {
+    console.log(`port http://localhost:${port}`);
+}
+
+app.listen(port, handleListening);
